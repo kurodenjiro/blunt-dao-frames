9d4621b2becbb50b2abff04e32585d8be1410109
kurodenjiro <kurodenjiro1@gmail.com>
2024-04-30 17:22:31 +0700

    remove edge

--- app/examples/new-api-slow-request/frames/frames.ts
+++
@@ -1,8 +0,0 @@
-import { createFrames } from "frames.js/next";
-import { appURL } from "../../../utils";
-
-export const frames = createFrames({
-  basePath: "/examples/new-api-slow-request/frames",
-  baseUrl: appURL(),
-  initialState: { isValidator: false },
-});
